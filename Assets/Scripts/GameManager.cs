f416b2555ae1f7accc3650636eb95e39bbcd65da
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2022-09-09 17:28:40 +0300

    Scripting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,13 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         if (instance == null)
+        {
             instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }//class
