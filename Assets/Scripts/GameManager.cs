f47e43ae4b3360a5826ef641b1b83bd2914e643b
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2022-09-09 17:12:06 +0300

    Edting Script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    [SerializeField] private GameObject[] characters;
+
+    private int _charIndex;
+
+    public int CharIndex
+    {
+        get { return _charIndex; }
+        set { _charIndex = value; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+}//class
